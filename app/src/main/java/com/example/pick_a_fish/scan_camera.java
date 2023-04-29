caa9a6475edf693852456e8568c334ed52f5ef0e
FinnowDude <130972141+FinnowDude@users.noreply.github.com>
2023-04-29 15:41:24 +0800

    Semi--Final Version 1.6

--- app/src/main/java/com/example/pick_a_fish/scan_camera.java
+++ app/src/main/java/com/example/pick_a_fish/scan_camera.java
@@ -1,14 +1,178 @@
 package com.example.pick_a_fish;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.pick_a_fish.ml.FishModel;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class scan_camera extends AppCompatActivity {
+
+    Button scan_images, gallery_image;
+    ImageView scanned_image;
+    TextView result_txt,result_txt2;
+    int imageSize = 224;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_camera);
+
+        scan_images = findViewById(R.id.scan_btn);
+        gallery_image = findViewById(R.id.gallery_btn);
+        scanned_image = findViewById(R.id.scan_img);
+        result_txt = findViewById(R.id.resultTxt);
+        result_txt2 = findViewById(R.id.resultTxt2);
+
+        //Take photo - Camera Open
+        scan_images.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 3);
+
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+
+                }
+
+            }
+        });
+
+        //Pick picture and open galley
+
+        gallery_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(cameraIntent, 1);
+            }
+        });
+
+
+
+
+    }
+
+    public void classifyImage(Bitmap image) {
+
+        try {
+            FishModel model = FishModel.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            for (int i = 0; i < imageSize; i++) {
+                for (int j = 0; j < imageSize; j++) {
+                    int val = intValues[pixel++];
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val & 0xFF) & 0xFF) * (1.f / 255.f));
+
+                }
+
+
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            FishModel.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidence = outputFeature0.getFloatArray();
+            //find the index of the class with the highest confidence
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidence.length; i++){
+                if(confidence[i] > maxConfidence){
+                    maxConfidence = confidence[i];
+                    maxPos = i;
+                }
+            }
+            String [] classes = {"Bangus","Bolinao","Dalagang Bukid","Galunggong","Hasa hasa","Maya maya","Sapsap","Tamban","Tilapia","Tulingan"};
+
+
+            float mxconfi = 0;
+
+            mxconfi = maxConfidence * 100;
+
+
+            if(mxconfi >= 70){
+
+                String str = Float.toString(mxconfi);
+                result_txt.setText(classes[maxPos]);
+                result_txt2.setText(str);
+
+
+            }else{
+                String str = Float.toString(mxconfi);
+                result_txt.setText("Couldn't Identify");
+                result_txt2.setText(str);
+            }
+
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 3){
+                Bitmap image = (Bitmap) data.getExtras().get("data");
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+                scanned_image.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }else{
+                Uri dat = data.getData();
+                Bitmap image = null;
+                try {
+                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                scanned_image.setImageBitmap(image);
+
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+                classifyImage(image);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
